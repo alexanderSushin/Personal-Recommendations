6a1631382d446f59a2396cb027ef81b651cc13b0
Andrey Pavlov <a.pavlov123123@gmail.com>
2022-12-16 11:30:53 +0300

    Update bot.py
    Added /start

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
 import telebot
 
 TOKEN = open('token.txt', 'r').read()
-bot = telebot.telebot(TOKEN=TOKEN)
+if TOKEN[-1] == '\n':
+	TOKEN = TOKEN[:-1]
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+@bot.message_handler(commands=["start"])
+def start (msg):
+	bot.send_message(msg.chat.id, 'Привет, это стартовое сообщение *редачится*')
 
 if __name__ == "__main__":
 	bot.infinity_polling()
